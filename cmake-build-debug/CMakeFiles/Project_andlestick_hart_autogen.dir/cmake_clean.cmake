9353fa6621d9af4b3a2239bcdb983402b25b6be9
Nikita Usoltsev <usoltsev-nv@yandex.ru>
2020-04-12 22:20:11 +0300

    deleted dir /cmake-build-debug

--- cmake-build-debug/CMakeFiles/Project_andlestick_hart_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Project_andlestick_hart_autogen"
-  "Project_andlestick_hart_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Project_andlestick_hart_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
